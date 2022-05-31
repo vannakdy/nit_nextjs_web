e474b9808654169a62bb824c8c69219d3c6dc4d5
Tola <100327396+khtola@users.noreply.github.com>
2022-05-31 22:08:12 +0700

    current

--- pages/component/masterpage/MasterPage.js
+++ pages/component/masterpage/MasterPage.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import style from "./MasterPage.module.css";
 import Image from 'next/image'
+import Link from 'next/link';
+import { useRouter } from 'next/router'
+import { BsArrowLeft } from "react-icons/bs";
+
 const MasterPage = (props) => {
+    const router = useRouter()
+
+    const handlBack = (e, path) => {
+        router.push(path)
+    }
     return (
         <div>
             <div className={style.header}>
+                <Link href={''}>
+                    <a onClick={(e) => handlBack(e, '/')}>
+                        <BsArrowLeft style={{ fontSize: 30, justifyContent: 'center', display: 'flex', paddingRight: 5 }} />
+                    </a>
+                </Link>
                 <div className={style.contain_logo}>
-                    <div style={{width:30,height:30,borderRadius:"100px",overflow: 'hidden'}}>
-                        <Image 
-                            src={require("../../assets/logo/INT_LOGO.png")} 
+                    <div style={{ width: 30, height: 30, borderRadius: "100px", overflow: 'hidden' }}>
+                        <Image
+                            src={require("../../assets/logo/INT_LOGO.png")}
                             alt="NIT Cambodai"
                         />
                     </div>
-                    <div className={style.text_brandname}>NIT Cambodia</div> 
+                    <div className={style.text_brandname}>NIT Cambodia</div>
                 </div>
-            </div> 
+            </div>
             <div className={style.body}>
                 {props.children}
-            </div> 
+            </div>
         </div>
     );
 }
--- pages/product_list.js
+++ pages/product_list.js
@@ -2,8 +2,53 @@ import React, { useState } from 'react';
 import Head from 'next/head';
 import { AiOutlineAppstoreAdd, AiOutlineAppstore } from "react-icons/ai";
 import MasterPage from './component/masterpage/MasterPage';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { Menu, Button } from 'antd';
+import 'antd/dist/antd.css';
+
+import {
+    AppstoreOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    PieChartOutlined,
+    DesktopOutlined,
+    ContainerOutlined,
+    MailOutlined,
+} from '@ant-design/icons';
+
+
+function getItem(label, key, icon, children, type) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+        type,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('Option 3', '3', <ContainerOutlined />),
+    getItem('Navigation One', 'sub1', <MailOutlined />, [
+        getItem('Option 5', '5'),
+        getItem('Option 6', '6'),
+        getItem('Option 7', '7'),
+        getItem('Option 8', '8'),
+    ]),
+    getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+        getItem('Option 9', '9'),
+        getItem('Option 10', '10'),
+        getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
+    ]),
+];
 function product_list(props) {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
     const [state, setState] = useState({
         iconOpen: true
     })
@@ -22,26 +67,28 @@ function product_list(props) {
             </Head>
             <MasterPage>
                 <h3>ព័ត៌មានថ្មី</h3>
-                <div className={iconOpen ? styles.Drawer:styles.DrawerClos}>
-                    <a onClick={handleOpen}>
-                        {iconOpen ? <AiOutlineAppstore style={{fontSize:30}}/> : <AiOutlineAppstoreAdd style={{fontSize:30}}/>}
-                    </a>
-                    {iconOpen && 
-                    <div className={styles.headerList}>
-                        <h3>មេរៀនទី១</h3>
-                        <div  className={styles.headerview}>
-                            <p>we are teach you about website</p>
-                        </div>
-                        <h3>មេរៀនទី២</h3>
-                        <div className={styles.headerview}>
-                            <p>we are teach you about website</p>
-                        </div>
-                        <h3>មេរៀនទី៣</h3>
-                        <div className={styles.headerview}>
-                            <p>we are teach you about website</p>
-                        </div>
-                    </div>
-                    }
+                <div
+                    style={{
+                        width: 256,
+                    }}
+                >
+                    <Button
+                        type="primary"
+                        onClick={toggleCollapsed}
+                        style={{
+                            marginBottom: 16,
+                        }}
+                    >
+                        {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                    </Button>
+                    <Menu
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        mode="inline"
+                        theme="dark"
+                        inlineCollapsed={collapsed}
+                        items={items}
+                    />
                 </div>
             </MasterPage>
         </div>
